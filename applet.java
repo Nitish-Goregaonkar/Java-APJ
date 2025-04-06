622aa1e65b7313ed8e1a6e5490e6a83dbba2d7eb
MarathaDada <goregaonkarnitish526@gmail.com>
2025-04-06 13:27:59 +0530

    java

---
+++ applet.java
@@ -0,0 +1,16 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.color;
+
+public class applet extends Applet {
+    public void paint(Graphics g) {
+        g.setcolor(Color.red);
+        g.fillRect(5, 5, 90, 90);
+        g.setcolor(Color.green);
+        g.fillRect(105, 5, 90, 90);
+        g.setcolor(Color.blue);
+        g.fillRect(5, 105, 90, 90);
+        g.setcolor(Color.yellow);
+        g.fillRect(105, 105, 90,90);
+    }
+}
